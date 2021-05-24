606116174d325886611c7ebf10a76db97d197c35
niahinti <niahinti@gmail.com>
2021-05-23 21:53:08 -0300

    generics

--- OblMarzo2021/src/Listas/ILista.java
+++ OblMarzo2021/src/Listas/ILista.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Listas;
 
-/**
- *
- * @author Camila
- */
 public interface ILista {
 
+    //POS: Constructor, crea una lista vacía
+    void Lista();
+
+    //POS: Retorna true si la lista no tiene nodos
+    boolean esVacia();
+
+    //POS: Inserta un nodo al principio de la lista
+    boolean agregarInicio(TDato n);
+
+    //POS: Inserta un nodo al final de la lista
+    boolean agregarFinal(TDato n);
+
+    //POS: Borra el primer nodo
+    boolean borrarInicio();
+
+    //POS: Borra el último nodo
+    boolean borrarFin();
+
+    //POS: Muestra los datos de la lista
+    void mostrar();
+
+    //POS: Elimina todos los nodos de la lista
+    void vaciar();
 }
--- OblMarzo2021/src/oblmarzo2021/OblMarzo2021.java
+++ OblMarzo2021/src/oblmarzo2021/OblMarzo2021.java
@@ -1,23 +1,23 @@
-
 package oblmarzo2021;
+
+import Dominio.*;
+import Listas.*;
 
 public class OblMarzo2021 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        Prueba p= new Prueba();
+        Prueba p = new Prueba();
         Obligatorio o = new Obligatorio();
-        juegodeprueba(p,o);
-        
+        juegodeprueba(p, o);
     }
-public static void juegodeprueba(Prueba p, Obligatorio o){
-    p.ver(o.crearSistemaReservas().resultado, Retorno.Resultado.OK, "se creo el sistema de reservas");
-    p.ver(o.registrarCiudad(1, "Monevideo").resultado, Retorno.Resultado.OK, "Se registro Montevideo");
-    p.ver(o.registrarCiudad(1, "Monevideo").resultado, Retorno.Resultado.ERROR, "Se intenta registrar Montevideo que ya existe");
-    
 
+    public static void juegodeprueba(Prueba p, Obligatorio o) {
+        p.ver(o.crearSistemaReservas().resultado, Retorno.Resultado.OK, "se creo el sistema de reservas");
+        p.ver(o.registrarCiudad(1, "Monevideo").resultado, Retorno.Resultado.OK, "Se registro Montevideo");
+        p.ver(o.registrarCiudad(1, "Monevideo").resultado, Retorno.Resultado.ERROR, "Se intenta registrar Montevideo que ya existe");
 
-    p.inicializarResultadosPrueba();
-}    
-    
+        p.inicializarResultadosPrueba();
+    }
+
 }
